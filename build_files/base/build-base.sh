02e125aa2558b096d99b20c492e8d5f18c9d959b
Garrick Welsh <garrick.welsh@gmail.com>
2024-07-06 00:05:50 +1000

    Merge from upstream.

--- build_files/base/build-base.sh
+++ build_files/base/build-base.sh
@@ -3,6 +3,8 @@
 
 set -ouex pipefail
 
+. /tmp/build/firmware.sh
+. /tmp/build/coreos_kernel.sh
 . /tmp/build/copr-repos.sh
 . /tmp/build/install-akmods.sh
 . /tmp/build/packages.sh
@@ -17,4 +19,5 @@ set -ouex pipefail
 . /tmp/build/aurora-changes.sh
 . /tmp/build/branding.sh
 . /tmp/build/initramfs.sh
+. /tmp/build/bootc.sh
 . /tmp/build/cleanup.sh
